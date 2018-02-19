dad98eefa78eef9ab126013138781c362375e359
dmromanov <battlebot98@gmail.com>
2018-02-20 00:56:55 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ import java.util.List;
 
 public class Main {
 
-    static List<Thread> threads = new ArrayList<>();
-    static final int NUMBEROFTHREADS = 5;
+    static List<Thread> threads = new ArrayList<>(); //потоки
+    static final int NUMBEROFTHREADS = 50; //количество потоков
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String task = null;
+
+        for (int i = 0; i < NUMBEROFTHREADS; i++)
+            threads.add(new Thread(new MyThread(String.valueOf(i)))); //добавление потоков в список
 
         while (true) {
             System.out.println("**********************" +
@@ -21,25 +24,22 @@ public class Main {
                     "\n**********************\n ");
 
             try {
-                task = reader.readLine();
+                task = reader.readLine(); //считывание команды с клавиатуры
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
             if (task.equals("1")) {
 
-                for (int i = 0; i < NUMBEROFTHREADS; i++) {
-                    threads.add(new Thread(new MyThread(String.valueOf(i))));
-                    threads.get(i).start();
-                }
-
-
                 for (Thread thread : threads)
-                    try {
-                        thread.join();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    thread.start();
+
+//                for (Thread thread : threads)
+//                    try {
+//                        thread.join(); // ожидание завершения потоков
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
 
             } else if (task.equals("2"))
                 break;
--- src/MyThread.java
+++ src/MyThread.java
@@ -3,27 +3,27 @@ import java.util.Random;
 
 public class MyThread implements Runnable {
 
-    public String ThreadName;
+    public String ThreadName; //название потока
 
-    public MyThread(String ThreadName) {
+    public  MyThread(String ThreadName) {
         this.ThreadName = ThreadName;
     }
 
     @Override
-    public void run() {
+    public  void run() {
 
-        System.out.println("Поток " + ThreadName + " выполняется");
+    System.out.println("Поток " + ThreadName + " выполняется");
 
-        int millisecond = 0;
+    int millisecond = 0;
 
-        try {
-            millisecond = new Random().nextInt(3000);
-            Thread.sleep(millisecond);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    try {
+        millisecond = new Random().nextInt(3000); //время выполнения потока(Получение рандомного числа)
+        Thread.sleep(millisecond); //поток засыпает
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
 
-        System.out.println("Поток " + ThreadName + " закончил выполнение за " + millisecond + " миллисекунд");
+    System.out.println("Поток " + ThreadName + " закончил выполнение за " + millisecond + " миллисекунд");
 
     }
 }
